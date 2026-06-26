5d6f1dee54f698271dec3611c0691fe1f481ab54
Yuri <yuri.victoria@edu.ufes.br>
2026-06-26 01:07:54 -0300

    ttyUSB* = rplidar

--- ros2_ws/src/Packages/rplidar_ros/scripts/create_udev_rules.sh
+++ ros2_ws/src/Packages/rplidar_ros/scripts/create_udev_rules.sh
@@ -5,7 +5,7 @@ echo "rplidar usb connection as /dev/rplidar, check it using the command : ls -l
 echo "start copy rplidar.rules to /etc/udev/rules.d/"
 source /usr/share/colcon_cd/function/colcon_cd.sh
 colcon_cd rplidar_ros
-sudo cp scripts/rplidar.rules  /etc/udev/rules.d
+sudo cp scripts/rplidar.rules  /etc/udev/rules.d/
 echo -e "\nRestarting udev\n"
 sudo service udev reload
 sudo service udev restart
